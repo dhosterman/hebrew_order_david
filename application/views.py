80f7df45e2b512c03a23422ed6b24828a9a2dff2
Daniel Hosterman <daniel.hosterman@gmail.com>
2014-12-14 15:19:13 -0600

    add basic form submission

--- application/views.py
+++ application/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect
+from django.db import transaction
+from accounts.models import User
 from application.forms import ContactDetailsForm, PersonalDetailsForm, \
     OtherDetailsForm, UserForm
 
@@ -13,22 +15,34 @@ def new(request):
     })
 
 
+@transaction.atomic
 def post(request):
     user = UserForm(request.POST)
     if user.is_valid():
-        print(user.cleaned_data)
+        user_instance = user.save(commit=False)
+        password = User.objects.make_random_password(length=8)
+        user_instance.set_password(password)
+        user_instance.save()
+    else:
+        print(user.errors)
 
     contact_details = ContactDetailsForm(request.POST)
     if contact_details.is_valid():
-        print(contact_details.cleaned_data)
+        contact_details_instance = contact_details.save(commit=False)
+        contact_details_instance.user = user_instance
+        contact_details_instance.save()
 
     personal_details = PersonalDetailsForm(request.POST)
     if personal_details.is_valid():
-        print(personal_details.cleaned_data)
+        personal_details_instance = personal_details.save(commit=False)
+        personal_details_instance.user = user_instance
+        personal_details_instance.save()
 
     other_details = OtherDetailsForm(request.POST)
     if other_details.is_valid():
-        print(other_details.is_valid)
+        other_details_instance = other_details.save(commit=False)
+        other_details_instance.user = user_instance
+        other_details_instance.save()
 
     return redirect('application.views.thank_you')
 
