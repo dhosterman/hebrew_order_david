151a03f27cffff807798aee9c3842ab804c8bc66
Daniel Hosterman <daniel.hosterman@gmail.com>
2014-12-29 11:42:38 -0600

    remove redundant settings

--- hebrew_order_david/settings.py
+++ hebrew_order_david/settings.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 AUTH_USER_MODEL = 'accounts.User'
 
@@ -100,14 +100,8 @@ if os.environ.get('DATABASE_URL'):
     # Honor the 'X-Forwarded-Proto' header for request.is_secure()
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-    # Allow all host headers
-    ALLOWED_HOSTS = ['*']
-
     # Static asset configuration
-    import os
-    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
     STATIC_ROOT = 'staticfiles'
-    STATIC_URL = '/static/'
 
     STATICFILES_DIRS = (
         os.path.join(BASE_DIR, 'static'),
