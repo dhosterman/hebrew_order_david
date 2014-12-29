0e169b0d14882e3680c5d0a1f926408a91c446cb
Daniel Hosterman <daniel.hosterman@gmail.com>
2014-12-29 13:52:23 -0600

    add possibility of blank tn to tn validation

--- application/validators.py
+++ application/validators.py
@@ -1,6 +1,11 @@
 import re
 
 
-def is_valid_tn(tn_string):
-    valid_tn = re.compile('^\d{3}-\d{3}-\d{4}$')
-    return re.match(valid_tn, tn_string)
+def is_valid_tn(tn_string, blank=False):
+    valid_tn = False
+    regex = re.compile('^\d{3}-\d{3}-\d{4}$')
+    if re.match(regex, tn_string):
+        valid_tn = True
+    if blank and not valid_tn and tn_string == '':
+        valid_tn = True
+    return valid_tn
