865aab89fdaef4af43d0a26f83197cb0a2802187
Daniel Hosterman <daniel.hosterman@gmail.com>
2015-03-25 00:44:50 -0400

    correct line breaks in email string

--- application/notify.py
+++ application/notify.py
@@ -5,9 +5,9 @@ def on_new_user(user):
     # notify administrators if a new user has completed an application
     admins = User.objects.filter(is_admin=True)    
     subject = 'New Application'
-    message = 'Applicant: {} {}/n'.format(user.first_name, user.last_name) 
-    message += 'Email: {}/n'.format(user.email)
-    message += 'Hebrew Name: {}/n'.format(user.hebrew_name)
+    message = 'Applicant: {} {}\r\n'.format(user.first_name, user.last_name) 
+    message += 'Email: {}\r\n'.format(user.email)
+    message += 'Hebrew Name: {}\r\n'.format(user.hebrew_name)
    
     for admin in admins:
         admin.email_user(subject=subject, message=message)
