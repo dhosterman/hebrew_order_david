828cb0b3361dbf909d07b0e3c1f6a1f2217d9993
Daniel Hosterman <daniel.hosterman@gmail.com>
2015-03-28 15:38:44 -0400

    add notification for updates

--- application/notify.py
+++ application/notify.py
@@ -11,3 +11,23 @@ def on_new_user(user):
    
     for admin in admins:
         admin.email_user(subject=subject, message=message)
+
+def on_updated_user(user, forms):
+    # notify adminstrators if an existing user has updated an application
+    admins = User.objects.filter(is_admin=True)
+    subject = 'Updated Application'
+    message = 'Applicant: {} {}\r\n'.format(user.first_name, user.last_name) 
+    message += 'Updated Fields:\r\n'
+    for form in forms:
+        if form.prefix:
+            message += '[{}]\r\n'.format(form.prefix)
+        for key in form.changed_data:
+            old_value = form.initial.get(key, 'blank')
+            new_value = form.cleaned_data.get(key, 'blank')
+            label = form.fields[key].label
+            message += '* {}: changed from {} to {}\r\n'.format(label,
+                                                              old_value,
+                                                              new_value)
+
+    for admin in admins:
+        admin.email_user(subject=subject, message=message)
